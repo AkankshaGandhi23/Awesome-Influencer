d7ebc962885444c94192130c3c9a0de69db79a83
m-triple-m <triplem656@gmail.com>
2024-04-14 18:48:41 +0530

    added details

--- backend/router/brandRouter.js
+++ backend/router/brandRouter.js
@@ -1,27 +1,33 @@
-const express = require ('express');
+const express = require('express');
 const router = express.Router();
 const Model = require('../model/brandModel')
 require('dotenv').config();
 const jwt = require('jsonwebtoken');
 
-router.post('/add',(req, res) => {
+router.post('/add', (req, res) => {
     console.log(req.body);
 
     //to save data in mongodb
     new Model(req.body).save()
-    .then((result) => {
-        res.status(200).json(result);  //200 mean successfull
-        
-    }).catch((err) => {
-        console.log(err);
-        res.status(500).json(err); 
-         // 500 means some error
-        
-    });
-    
+        .then((result) => {
+            res.status(200).json(result);  //200 mean successfull
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
 })
-router.get('/getall',(req , res) => {
-    res.send('post read operation');
+router.get('/getall', (req, res) => {
+    Model.find()
+        .then((result) => {
+            res.status(200).json(result);
+
+        }).catch((err) => {
+            console.log(err);
+            res.status(500).json(err);
+
+        });
 });
 
 router.post("/authenticate", (req, res) => {
--- backend/router/campaignRouter.js
+++ backend/router/campaignRouter.js
@@ -28,5 +28,15 @@ router.get('/getall',(req , res) => {
     });
 });
 
+router.get('/getbyid/:id',(req , res) => {
+    Model.findById(req.params.id)
+    .then((result) => {
+        res.status(200).json(result);
+    }).catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
 
 module.exports = router;
